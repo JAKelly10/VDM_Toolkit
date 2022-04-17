8921276199c756f39b2337ff13676d816bca3e87
Jack Kelly <b9003615@newcastle.ac.uk>
2022-04-17 01:11:46 +0100

    added identifier designator which should be all of the classes needed to translate assignment

---
+++ plugins/vdm2isa/java/src/main/java/vdm2isa/tr/expressions/TRIdentifierDesignator.java
@@ -0,0 +1,59 @@
+package vdm2isa.tr.expressions;
+
+import vdm2isa.lex.IsaToken;
+import vdm2isa.lex.TRIsaVDMCommentList;
+import vdm2isa.messages.IsaErrorMessage;
+import vdm2isa.messages.IsaInfoMessage;
+import vdm2isa.messages.IsaWarningMessage;
+import vdm2isa.tr.TRNode;
+import vdm2isa.tr.definitions.TRExplicitFunctionDefinition;
+import vdm2isa.tr.expressions.TRExpression;
+import vdm2isa.tr.expressions.visitors.TRExpressionVisitor;
+import vdm2isa.tr.expressions.TRStateDesignator;
+import vdm2isa.tr.patterns.TRMultipleBindList;
+import vdm2isa.tr.patterns.TRPattern;
+import vdm2isa.tr.patterns.TRPatternListList;
+import vdm2isa.tr.patterns.TRPatternContext;
+import vdm2isa.tr.types.TRAbstractInnerTypedType;
+import vdm2isa.tr.types.TRInvariantType;
+import vdm2isa.tr.types.TRMapType;
+import vdm2isa.tr.types.TROptionalType;
+import vdm2isa.tr.types.TRVoidType;
+import vdm2isa.tr.types.TRSeqType;
+import vdm2isa.tr.types.TRSetType;
+import vdm2isa.tr.types.TRType;
+import vdm2isa.tr.types.TRUnknownType;
+
+import com.fujitsu.vdmj.lex.LexLocation;
+import com.fujitsu.vdmj.tc.definitions.TCDefinitionList;
+import com.fujitsu.vdmj.tc.statements.TCIdentifierDesignator;
+import com.fujitsu.vdmj.tc.expressions.visitors.TCFreeVariableExpressionVisitor;
+import com.fujitsu.vdmj.tc.lex.TCNameToken;
+import com.fujitsu.vdmj.tc.lex.TCNameSet;
+import com.fujitsu.vdmj.typechecker.Environment;
+import com.fujitsu.vdmj.typechecker.FlatEnvironment;
+
+public class TRIdentifierDesignator extends TRStateDesignator {
+
+    public final TCNameToken name;
+    
+    public TRIdentifierDesignator(LexLocation location, TCNameToken name)
+	{
+		super(location);
+        this.name = name;
+	}
+
+    public <R, S> R apply(TRExpressionVisitor<R, S> visitor, S arg) 
+    {
+        return visitor.caseTRStateDesignator(this, arg);
+    }
+
+    public String translate(){
+        return "identifier";
+    }
+
+    public IsaToken isaToken() {
+        return IsaToken.EOF;
+    }
+
+}
