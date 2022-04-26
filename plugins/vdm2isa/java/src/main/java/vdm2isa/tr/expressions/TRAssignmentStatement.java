9ef0ae51237ec8ccc21f10ce34741a56ef24f99d
Jack Kelly <b9003615@newcastle.ac.uk>
2022-04-26 13:24:02 +0100

    added seqcomp

--- plugins/vdm2isa/java/src/main/java/vdm2isa/tr/expressions/TRAssignmentStatement.java
+++ plugins/vdm2isa/java/src/main/java/vdm2isa/tr/expressions/TRAssignmentStatement.java
@@ -65,8 +65,8 @@ public class TRAssignmentStatement extends TRStatement {
         // }
         //System.out.println(TRStateDefinition.state.recordType.getInvDef().getParameters().patternContextTranslate(null));
         // Check Parattern context is the correct dummy?
-        return TRStateDefinition.state.recordType.getInvDef().getParameters().patternContextTranslate(null) + "\n" + 
-            target.translate() + " Assignemnt " + exp.translate();
+        return TRStateDefinition.state.recordType.getInvDef().getParameters().patternContextTranslate(null) + "\n\t" + "seqcomp" +
+        IsaToken.SPACE.toString() + target.translate() + IsaToken.SPACE.toString() + exp.translate();
     }
 
     public IsaToken isaToken() {
